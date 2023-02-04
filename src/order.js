3b6289b250f2a2db81c92e7735c1ec50645703c6
Tiala Young <tialayoung@gmail.com>
2023-02-03 18:55:29 -0700

    Finish order file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,48 @@
+function takeOrder(newOrder, ordersList) {
+  if (ordersList.length < 3) {
+  return ordersList.push(newOrder);
+  } else {
+    return ordersList;
+  };
+};
+
+
+function refundOrder(id, ordersList) {
+  for (var i = 0; i < ordersList.length; i++) {
+    if (ordersList[i].orderNumber === id) {
+      ordersList.splice(i,1);
+    };
+  };
+
+  return ordersList;
+}
+
+
+function listItems(ordersList) {
+  var itemsList = [];
+
+  for (var i = 0; i < ordersList.length; i++) {
+    itemsList.push(ordersList[i].item)
+  };
+
+  return itemsList.join(", ");
+};
+
+
+function searchOrder(ordersList, search) {
+  for (var i = 0; i < ordersList.length; i++) {
+    if (ordersList[i].item.includes(search) === true) {
+      return true;
+    };
+  };
+
+  return false;
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
