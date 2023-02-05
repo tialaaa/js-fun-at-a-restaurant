dc94d12f55607eb7221a5457f389a7ea59e0ddaa
Tiala Young <tialayoung@gmail.com>
2023-02-04 18:56:41 -0700

    Finish Chef file

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,28 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+
+  greetCustomer(customerName, morningHours) {
+    if (morningHours === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`;
+    }
+  }
+
+  checkForFood(foodItem) {
+    var matchedMenuArray = this.restaurant.menus[`${foodItem.type}`];
+
+      for (var i = 0; i < matchedMenuArray.length; i++) {
+        if (matchedMenuArray[i] === foodItem) {
+          return `Yes, we're serving ${foodItem.name} today!`;
+        };
+      };
+      
+    return `Sorry, we aren't serving ${foodItem.name} today.`;
+  }
+};
 
 module.exports = Chef;
